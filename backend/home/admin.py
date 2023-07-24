075a3bef29a57eedc898637d331fdecac9721900
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-24 07:55:39 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bder
+admin.site.register(Bder)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Bder(models.Model):
+    'Generated Model'
+    cmcm = models.BigIntegerField()
+    pmpm = models.BigIntegerField()
